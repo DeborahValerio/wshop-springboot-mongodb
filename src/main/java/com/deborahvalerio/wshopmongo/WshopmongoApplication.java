23b6501041081c7d0996f52d9befcc4ff02db961
DeborahValerio <dborah.v@gmail.com>
2023-04-26 16:36:36 -0300

    Project created

---
+++ src/main/java/com/deborahvalerio/wshopmongo/WshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.deborahvalerio.wshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deborahvalerio/wshopmongo/WshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deborahvalerio.wshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
