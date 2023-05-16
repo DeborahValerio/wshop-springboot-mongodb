c70b0baa83fe8ed81e89f652a89a1f5a0ad19813
DeborahValerio <dborah.v@gmail.com>
2023-05-16 16:04:37 -0300

    Post findById

---
+++ src/main/java/com/deborahvalerio/wshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.deborahvalerio.wshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deborahvalerio.wshopmongo.domain.Post;
+import com.deborahvalerio.wshopmongo.repositories.PostRepository;
+import com.deborahvalerio.wshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository rep;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = rep.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
